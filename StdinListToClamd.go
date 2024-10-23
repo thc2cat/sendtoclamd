a411c1e0f0c35e1e6d57df36351d59f6711b953f
thc2cat <thc2cat+github@gmail.com>
2024-10-23 11:22:00 +0200

    init

--- StdinListToClamd.go
+++ StdinListToClamd.go
@@ -6,29 +6,35 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/thc2cat/go-clamd"
 )
 
-func main() {
+var (
+	clamavHostPtr        = flag.String("h", "clamd.inyourdns.net", "remote clamd host")
+	clamavPortPtr        = flag.Int("p", 3310, "clamd port to connect")
+	defaultMaxStreamSize = (int64)(52428800)
+)
 
-	clamavHostPtr := flag.String("host", "lorraine.ens.uvsq.fr", "Adresse IP du démon clamd")
-	clamavPortPtr := flag.Int("port", 3310, "Port du démon clamd")
+// StreamMaxLength 50M
+// You also have to change the ClamClient instance with a higher MaxStreamSize:
+
+// var client = new ClamClient("localhost", 3310)
+//
+//	{
+//		     MaxStreamSize = 52428800
+//		};
+func main() {
 
 	flag.Parse()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
-	defer cancel()
+	// ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	ctx := context.TODO()
+	// defer cancel()
 
-	c := clamd.NewClamd(clamd.WithTCP(*clamavHostPtr, *clamavPortPtr))
-	if c == nil {
-		fmt.Printf("Erreur lors de la création du client ClamAV\n")
-		return
-	}
+	c := NewClamd()
 
-	if ok, cerr := c.Ping(ctx); !ok {
-		fmt.Println("clamd server error !", cerr)
+	if !PingClamdOk(c, ctx) {
 		os.Exit(-1)
 	}
 
@@ -43,18 +49,56 @@ func main() {
 
 }
 
+func NewClamd() *clamd.Clamd {
+	c := clamd.NewClamd(clamd.WithTCP(*clamavHostPtr, *clamavPortPtr))
+	if c == nil {
+		fmt.Fprintf(os.Stderr, "Error in clamd.NewClamd(clamd.WithTCP(host, port)) \n")
+	}
+	return c
+}
+func PingClamdOk(c *clamd.Clamd, ctx context.Context) bool {
+	if ok, cerr := c.Ping(ctx); !ok {
+		fmt.Fprintf(os.Stderr, "503 Clamd Ping  error : %v !", cerr)
+		return false
+	}
+	return true
+}
+
+func CheckFilesize(thePath string) error {
+	fileInfo, err := os.Stat(thePath)
+	if err != nil {
+		return fmt.Errorf("404 File vanished: %s %v", thePath, err)
+	}
+	if fileInfo.Size() > defaultMaxStreamSize {
+		return fmt.Errorf("413 File size of %s is larger (%d) than default Max Stream Size (%v)", thePath, fileInfo.Size(), defaultMaxStreamSize)
+	}
+	if fileInfo.Mode().IsDir() {
+		return nil
+	}
+	return nil
+}
+
 // Fonction pour envoyer un fichier vers l'antivirus
 func sendToClamav(path string, c *clamd.Clamd, ctx context.Context) error {
 
+	for retries := 3; !PingClamdOk(c, ctx) && retries > 0; retries-- {
+		fmt.Fprintf(os.Stderr, "503 Clamd server error retrying")
+		c = NewClamd()
+	}
+
 	if ok, cerr := c.Ping(ctx); !ok {
-		fmt.Println("clamd server error !", cerr)
+		fmt.Fprintf(os.Stderr, "503 Clamd server error : %v !", cerr)
 		return cerr
 	}
 
+	if errCkFS := CheckFilesize(path); errCkFS != nil {
+		return nil // This is not a Clamav ERROR but acces error
+	}
+
 	file, err := os.Open(path)
-	if err != nil {
-		fmt.Println(err)
-		return err
+	if err != nil { // Specifics errors ( bad links and others )
+		// fmt.Fprintln(os.Stderr, err)
+		return nil
 	}
 	defer file.Close()
 
---
+++ go.mod
@@ -0,0 +1,5 @@
+module projects/sendtoclamd
+
+go 1.23.0
+
+require github.com/thc2cat/go-clamd v0.0.0-20240828120855-3d61ed93a1a8
